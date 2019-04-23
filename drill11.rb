7b2d66a053e8d54d273afc245d1caf2d177fe242
keisukekodama <myzk.kodama@gmail.com>
2019-04-23 10:15:47 +0900

    Create drill11.rb

---
+++ drill11.rb
@@ -0,0 +1,11 @@
+# 問題文：
+# 入力した1~10の範囲内であればtureを出力。outside_modeがtrueで無い限り
+# それ以外は偽のプログラムを作りましょう
+# 概要：
+# in1to10（num、outside_mode）→出力
+# in1to10（5、False）→True 
+# in1to10（11、False）→False 
+# in1to 10（11、True）→True
+
+
+#模範解答
