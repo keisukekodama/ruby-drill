92dfef6c6f51dc85a3e51af6d493337d68bf2f93
keisukekodama <myzk.kodama@gmail.com>
2019-04-24 09:25:09 +0900

    Update drill04.rb

--- drill04.rb
+++ drill04.rb
@@ -1,9 +1,11 @@
 # 問題文：家におしゃべりなオウムを飼っています。
 # オウムが喋っている時間が昼間だと
 # 問題は無いのですが、7時前や20時以降だと問題があります。
-# 喋っている時間が7時前や20時以降だと
-# False、それ以外はTrueと返しましょう。
-# 概要：
+# 喋っている時間が7時前や20時以降だと問題ありで
+# True、それ以外は問題なしなのでFalseと返しましょう。
+
+# 出力例：
+# parrot_trouble(talk, time) 
 # parrot_trouble(True, 6) → True
 # parrot_trouble(True, 7) → False
 # parrot_trouble(False, 6) → False
