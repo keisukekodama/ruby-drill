58f2714a6c48f6250c0a72a7f0a0691179bc6a0e
keisukekodama <myzk.kodama@gmail.com>
2019-04-24 09:25:06 +0900

    Update drill03.rb

--- drill03.rb
+++ drill03.rb
@@ -1,10 +1,20 @@
 # 問題文：
-# 文字を入力後、数字を入力します。
-# 入力した文字に対して入力したn番目文字を消すプログラムを作りましょう
-# 概要：
+# 文字に対してn番目の文字を消すプログラムを作りましょう
+
+# 出力例：
 # missing_char('kitten', 1) → 'ktten'
 # missing_char('kitten', 0) → 'itten'
 # missing_char('kitten', 4) → 'kittn'
 
 
 #模範解答
+
+def missing_char(str, n):
+  front = str[]   
+  back = str[]  
+  return front + back
+
+ str =[k,i,t,t,e,n]
+ n = 2
+
+  missing_char(str, n)
