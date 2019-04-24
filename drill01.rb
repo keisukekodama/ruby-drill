b98d48793652cb8bd63ff7d343eefb5211e4deb9
keisukekodama <myzk.kodama@gmail.com>
2019-04-25 08:48:56 +0900

    Update drill01.rb

--- drill01.rb
+++ drill01.rb
@@ -1,23 +1,24 @@
 #問題文：休日か休日でないかで返ってくるメッセージを変更するプログラムを作成しましょう。
-#平日でないまたは休日の場合は「良い休日を！」と返信し、
-#休日の場合は「今日も一日がんばりましょう！」と条件分岐させましょう。
+#平日でないまたは休日の場合は「True」と返信し、
+#休日の場合は「False」と条件分岐させましょう。
+
 #出力例：
+# sleep_in(weekday, vacation)
 # sleep_in(False, False) → True
 # sleep_in(True, False) → False
 # sleep_in(False, True) → True
 
 #模範解答
 def sleep_in(weekday, vacation)
-  if (weekday != "yes") || (vacation == "yes" )
-    puts "良い休日を！"
+  if (weekday != "True") || (vacation == "True" )
+    puts "True"
   else
-    puts "今日も一日がんばりましょう！"
+    puts "False"
   end
 end
 
-weekday = "no"
-
-vacation = "no"
+weekday = "False"
+vacation = "False"
 
 sleep_in(weekday,vacation)
 
