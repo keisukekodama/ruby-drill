3659d3b743e6717609aea3e3724a40c1da77a700
keisukekodama <myzk.kodama@gmail.com>
2019-04-26 13:56:04 +0900

    first

---
+++ drill02.rb
@@ -0,0 +1,24 @@
+#問題文：アタナは警官です。aとｂ二人の容疑者の取り調べをしています。二人の証言が
+# どちらも証言がTrue、またはFalseであればその証言はTrueです。
+# しかしどちらかがFalseでTrueであればその証言はFalseと
+# 出力するプログラムを作りましょう
+
+#出力例：
+# police_trouble(a, b)
+# police_trouble(True, True) → True
+# police_trouble(False, False) → True
+# police_trouble(True, False) → False
+
+#模範解答
+def police_trouble(a, b)
+  if ((a == "Ture") && (b == "Ture" )|| (a == "False") && (b == "False" ))
+    puts "Ture"
+  else
+    puts "False"
+  end
+end
+
+a = "Ture"
+b = "Ture"
+
+police_trouble(a,b)
---
+++ drill04.rb
@@ -0,0 +1,12 @@
+# 問題文：家におしゃべりなオウムを飼っています。
+# 喋っている時間が7時前や20時以降だと問題がありTrue、
+# それ以外の時間帯はオウムが喋っていても
+# 問題なしなのでFalseと出力するプログラムを作りましょう。
+
+# 出力例：
+# parrot_trouble(talk, time) 
+# parrot_trouble(True, 6) → True
+# parrot_trouble(True, 7) → False
+# parrot_trouble(False, 6) → False
+
+#模範解答
